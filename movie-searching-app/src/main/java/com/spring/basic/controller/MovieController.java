a447151b25c0f1b39f38e176534162ecf55a2652
MuthuAnoud <muthukumar.m@anoudtechnologies.com>
2023-01-30 21:05:16 +0530

    Movie Searching Application Added.

---
+++ movie-searching-app/src/main/java/com/spring/basic/controller/MovieController.java
@@ -0,0 +1,38 @@
+package com.spring.basic.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.basic.service.MovieService;
+
+/**
+ * @author MayilaiMuthu
+ * @apiNote 30-01-2023
+ *
+ */
+@RestController
+@RequestMapping("/movie")
+public class MovieController {
+	
+    @Autowired
+    MovieService movieService;
+    
+    @GetMapping("/check")
+    public String homeController(){
+        return "moviedetails";
+    }
+
+    @GetMapping("/search")
+    public String getMovieByName(@RequestParam String title, Model model) {
+        try {
+			model.addAttribute("movies", movieService.getMovieByName(title));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        return "moviedetails";
+    }
+}
