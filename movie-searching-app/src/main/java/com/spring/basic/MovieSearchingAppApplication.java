2a15684bb3f9835d05520b5862500eeb636584cb
MuthuAnoud <muthukumar.m@anoudtechnologies.com>
2023-01-30 21:11:13 +0530

    Test.....

---
+++ movie-searching-app/src/main/java/com/spring/basic/MovieSearchingAppApplication.java
@@ -0,0 +1,18 @@
+package com.spring.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author MayilaiMuthu
+ * @apiNote 30-01-2023
+ *
+ */
+@SpringBootApplication
+public class MovieSearchingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieSearchingAppApplication.class, args);
+	}
+
+}
