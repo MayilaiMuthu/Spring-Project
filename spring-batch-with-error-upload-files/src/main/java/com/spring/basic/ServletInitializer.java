8f12b8e3912307602de2822f5b334a5f524c5f02
Muthu <muthukumar.m@DT150157.QICHOAPPL.com>
2023-03-21 21:14:21 +0530

    commit test.....

--- spring-batch-with-error-upload-files/src/main/java/com/spring/basic/ServletInitializer.java
+++ spring-batch-with-error-upload-files/src/main/java/com/spring/basic/ServletInitializer.java
@@ -3,6 +3,11 @@ package com.spring.basic;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author MayilaiMuthu
+ * @apiNote 21-03-2023
+ *
+ */
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
--- spring-batch-with-error-upload-files/src/main/java/com/spring/basic/SpringBatchWithErrorUploadFilesApplication.java
+++ spring-batch-with-error-upload-files/src/main/java/com/spring/basic/SpringBatchWithErrorUploadFilesApplication.java
@@ -3,6 +3,11 @@ package com.spring.basic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author MayilaiMuthu
+ * @apiNote 21-03-2023
+ *
+ */
 @SpringBootApplication
 public class SpringBatchWithErrorUploadFilesApplication {
 
