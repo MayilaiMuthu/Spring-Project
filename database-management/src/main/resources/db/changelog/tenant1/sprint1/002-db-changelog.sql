fb98b5a9e1c2467ecdd3a156a31bc29f61cbd1ae
MuthuAnoud <muthukumar.m@anoudtechnologies.com>
2023-10-13 09:54:53 +0530

    changes added.....

--- database-management/src/main/resources/db/changelog/tenant1/sprint1/002-db-changelog.sql
+++ database-management/src/main/resources/db/changelog/tenant1/sprint1/002-db-changelog.sql
@@ -1,19 +1,6 @@
 -- liquibase formatted sql
 
--- changeset muthu:1
-CREATE TABLE test_table_1 (
-    test_column_1 VARCHAR(255) NOT NULL
-);
--- rollback DROP TABLE test_table_1
-
--- changeset muthu:2
-CREATE TABLE test_table_2 (
-    test_column_1 VARCHAR(255) NOT NULL
-);
--- rollback DROP TABLE test_table_2
-
--- changeset muthu:3
-CREATE TABLE test_table_3 (
-    test_column_1 VARCHAR(255) NOT NULL
-);
--- rollback DROP TABLE test_table_3
+-- changeset muthu:4
+-- changesetId: 1
+DROP TABLE test_table_1;
+-- rollback CREATE TABLE test_table_1 (test_column_1 VARCHAR(255) NOT NULL);
